db1972971fd3163050b87f820059ca4a6bbfeabc
BoManev <bomanev@pm.me>
2023-08-22 00:04:52 -0400

    print panic context

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,12 @@ use core::panic::PanicInfo;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     print!("Hello World{}", "!");
+    panic!("Panicing...");
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
